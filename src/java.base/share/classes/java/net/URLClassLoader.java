ced57fe9793b3dd14eef480f9651466502f0ca6c
jpai <jaikiran.pai@gmail.com>
2021-11-23 20:13:13 +0530

    8205976: wip

--- src/java.base/share/classes/java/net/URLClassLoader.java
+++ src/java.base/share/classes/java/net/URLClassLoader.java
@@ -293,15 +293,13 @@ public class URLClassLoader extends SecureClassLoader implements Closeable {
                 return null;
             }
             URLConnection urlc = url.openConnection();
-            InputStream is = urlc.getInputStream();
             if (urlc instanceof JarURLConnection juc) {
-                JarFile jar = juc.getJarFile();
-                synchronized (closeables) {
-                    if (!closeables.containsKey(jar)) {
-                        closeables.put(jar, null);
-                    }
-                }
-            } else if (urlc instanceof sun.net.www.protocol.file.FileURLConnection) {
+                // don't use JarFile cache
+                juc.setUseCaches(false);
+            }
+            InputStream is = urlc.getInputStream();
+            if (urlc instanceof sun.net.www.protocol.file.FileURLConnection
+                    || urlc instanceof JarURLConnection) {
                 synchronized (closeables) {
                     closeables.put(is, null);
                 }
