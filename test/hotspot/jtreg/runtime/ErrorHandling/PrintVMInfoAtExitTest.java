e25c3699c87f0ab202346a7369c0592c355184f5
Jaikiran Pai <jaikiran.pai@gmail.com>
2025-06-23 14:17:29 +0530

    8359830: update the PrintVMInfoAtExitTest test to even verify the macOS version reported in the output

--- test/hotspot/jtreg/runtime/ErrorHandling/PrintVMInfoAtExitTest.java
+++ test/hotspot/jtreg/runtime/ErrorHandling/PrintVMInfoAtExitTest.java
@@ -28,11 +28,13 @@
  * @summary Test PrintVMInfoAtExit
  * @library /test/lib
  * @modules java.base/jdk.internal.misc
+ *          java.base/jdk.internal.util
  * @requires vm.flagless
  * @requires vm.bits == "64"
  * @run driver PrintVMInfoAtExitTest
  */
 
+import jdk.internal.util.OperatingSystem;
 import jdk.test.lib.process.OutputAnalyzer;
 import jdk.test.lib.process.ProcessTools;
 
@@ -54,6 +56,36 @@ public class PrintVMInfoAtExitTest {
     output_detail.shouldContain("Command Line: -Xmx64M -Xms64M -XX:-CreateCoredumpOnCrash -XX:+UnlockDiagnosticVMOptions -XX:+PrintVMInfoAtExit -XX:NativeMemoryTracking=summary -XX:CompressedClassSpaceSize=256m");
     output_detail.shouldContain("Native Memory Tracking:");
     output_detail.shouldContain("Java Heap (reserved=65536KB, committed=65536KB)");
+
+    // -XX:+PrintVMInfoAtExit prints the host's OS name and version too. Each OS reports it
+    // differently. Currently we assert the OS version only for macOS since that OS has occasionally
+    // reported two different values for the same OS version. We expect the text in the output
+    // to contain the OS version that matches the Java system property value for "os.version".
+    if (OperatingSystem.isMacOS()) {
+      System.out.println("verifying the reported OS version");
+      // -XX:+PrintVMInfoAtExit prints to STDOUT
+      assertMacOSVersion(output_detail.getStdout());
+    }
+  }
+
+  // verifies that the macOS version reported in the output of -XX:+PrintVMInfoAtExit
+  // matches the value of os.version Java system property
+  private static void assertMacOSVersion(final String output) {
+    String hostLine = null;
+    for (final String line : output.split("\\n")) {
+      if (line.startsWith("Host: ")) {
+        hostLine = line;
+        break;
+      }
+    }
+    if (hostLine == null) {
+      throw new AssertionError("Missing \"Host: \" line in the output");
+    }
+    final String expectedOSVersion = System.getProperty("os.version");
+    final String expectedContent = " macOS " + expectedOSVersion + " ";
+    if (!hostLine.contains(expectedContent)) {
+      throw new AssertionError("\"" + expectedContent + "\" is missing in \"" + hostLine + "\"");
+    }
   }
 }
 
